91a15d7fc163cf33e28a1177754c278d9ede036e
PrOn1x_ <101285149+Pr0n1xGH@users.noreply.github.com>
2024-10-28 20:05:03 +0500

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -1,44 +1,63 @@
 using System;
-using System.Reflection;
 
-namespace Name
-
+namespace Calculator
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите число1 - ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Введите знак(+,-,*,/) - ");
-            char znak = Convert.ToChar(Console.ReadLine());
-
-            Console.WriteLine("Введите число2 - ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-
-            int result = num1 + num2;
-
-
-            if (znak == '+') 
+            try
             {
-                result = num1 + num2;
-            }
-            else if (znak == '-')
-            {
-                result = num1 - num2;
-            }
-            else if (znak == '*')
-            {
-                result = num1 * num2;
-            }
-            else if (znak == '/')
-            {
-                result = num1 / num2;
-            }
-            Console.WriteLine($"У вас получилось : {num1} {znak} {num2} = {result}");
+                Console.Write("Введите первое число: ");
+                if (!int.TryParse(Console.ReadLine(), out int firstNumber))
+                {
+                    throw new ArgumentException("Введено некорректное число");
+                }
 
-            Console.ReadKey();
+                Console.Write("Введите операцию (+, -, *, /): ");
+                char operation = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                if (!"+-*/".Contains(operation))
+                {
+                    throw new ArgumentException("Некорректная операция");
+                }
+
+                Console.Write("Введите второе число: ");
+                if (!int.TryParse(Console.ReadLine(), out int secondNumber))
+                {
+                    throw new ArgumentException("Введено некорректное число");
+                }
+
+                if (operation == '/' && secondNumber == 0)
+                {
+                    throw new DivideByZeroException("Деление на ноль невозможно");
+                }
+
+                int result = Calculate(firstNumber, secondNumber, operation);
+                Console.WriteLine($"Результат: {firstNumber} {operation} {secondNumber} = {result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("\nНажмите любую клавишу для выхода...");
+                Console.ReadKey();
+            }
+        }
+
+        private static int Calculate(int a, int b, char operation)
+        {
+            return operation switch
+            {
+                '+' => a + b,
+                '-' => a - b,
+                '*' => a * b,
+                '/' => a / b,
+                _ => throw new ArgumentException("Неподдерживаемая операция")
+            };
         }
     }
 }
